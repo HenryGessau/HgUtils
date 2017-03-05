bbc8b88e48572d056ef3e3c621120b9db59db7fd
Henry Gessau <HenryG@gessau.net>
2017-03-05 01:52:46 -0500

    Created

---
+++ dedup_bash_hist.py
@@ -0,0 +1,62 @@
+import argparse
+import tqdm
+
+
+def pairwise(iterable):
+    a = iter(iterable)
+    return zip(a, a)
+
+
+def remove_dups(history):
+    from_end = 1
+    total_dups = 0
+    with tqdm.tqdm(total=len(history)-1) as progress:
+        while from_end < len(history):
+            target = len(history) - from_end
+            command = history[target][0]
+            dups = 0
+            for i in range(target - 1, -1, -1):
+                if history[i][0] == command:
+                    del history[i]
+                    dups += 1
+                    progress.update(1)
+            total_dups += dups
+            from_end += 1
+            progress.update(1)
+    print("Removed %d duplicates" % total_dups)
+    return total_dups
+
+
+def save_to_file(history, filename):
+    filename += '.deduped'
+    with open(filename, 'w') as f:
+        for command, date in history:
+            f.write('%s\n%s\n' % (date, command))
+    print("Wrote to", filename)
+
+
+def main(filename, minlen):
+    with open(filename) as f:
+        lines = f.read().splitlines()
+        history = [(command.strip(), date)
+                   for date, command in pairwise(lines)]
+    if minlen:
+        shorts = 0
+        for i in range(len(history) - 1, -1, -1):
+            if len(history[i][0]) < minlen:
+                del history[i]
+                shorts += 1
+        print("Removed %d commands shorter than %d" % (shorts, minlen))
+    remove_dups(history)
+    save_to_file(history, filename)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Bash history file de-duplicator')
+    parser.add_argument('-m', action="store", dest="minlen", type=int,
+                        default=0, help='Minimum command length to save')
+    parser.add_argument('history_file', action="store",
+                        type=argparse.FileType('rt'))
+    results = parser.parse_args()
+    main(results.history_file.name, results.minlen)
