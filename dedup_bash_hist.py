1ca7715aa2c072c228e24636ef0558e73aa8151c
Henry Gessau <HenryG@gessau.net>
2019-05-11 21:58:39 -0400

    Refactor

--- dedup_bash_hist.py
+++ dedup_bash_hist.py
@@ -1,34 +1,47 @@
 import argparse
-import tqdm
+from typing import List, Tuple, Iterable
 
 
-def pairwise(iterable):
+def pairwise(iterable) -> Iterable[Tuple[str, str]]:
     a = iter(iterable)
     return zip(a, a)
 
 
-def remove_dups(history):
-    from_end = 1
+def normalize_history(lines: List[str]) -> List[str]:
+    c = 0
+    last = len(lines) - 1
+    history: List[str] = []
+    while c < last:
+        if not lines[c].startswith('#'):
+            # Multi-line command
+            history[-1] += '\n' + lines[c]
+            c += 1
+            continue
+        history.append(lines[c])  # date
+        c += 1
+        history.append(lines[c])  # command
+        c += 1
+    return history
+
+
+def remove_dups(history: List[Tuple[str, str]]) -> List[Tuple[str, str]]:
     total_dups = 0
-    print(f"Checking {len(history)} commands for duplicates...")
-    with tqdm.tqdm(total=len(history)-1) as progress:
-        while from_end < len(history):
-            target = len(history) - from_end
-            command = history[target][0]
-            dups = 0
-            for i in range(target - 1, -1, -1):
-                if history[i][0] == command:
-                    del history[i]
-                    dups += 1
-                    progress.update()
-            total_dups += dups
-            from_end += 1
-            progress.update()
+    shistory = sorted(history, key=lambda c: c[0])
+    print(f"Removing duplicates ...")
+    uhistory: List[Tuple[str, str]] = []
+    for i in range(len(shistory) - 1):
+        if shistory[i][0] == shistory[i+1][0]:
+            total_dups += 1
+        else:
+            uhistory.append(shistory[i])
+    uhistory.append(shistory[-1])
+    assert len(uhistory) == len(shistory) - total_dups
     print(f"Removed {total_dups} duplicates")
-    return total_dups
+    dhistory = sorted(uhistory, key=lambda d: d[1])
+    return dhistory
 
 
-def save_to_file(history, filename):
+def save_to_file(history: List[Tuple[str, str]], filename: str):
     filename += '.deduped'
     with open(filename, 'w') as f:
         for command, date in history:
@@ -36,23 +49,21 @@ def save_to_file(history, filename):
     print("Wrote to", filename)
 
 
-def main(filename, minlen):
+def main(filename: str, minlen: int):
     with open(filename) as f:
-        lines = f.read().splitlines()
-        history = [(command.strip(), date)
-                   for date, command in pairwise(lines)]
-    print(f"Processing {filename} with {len(history)} commands")
+        lines = normalize_history(f.read().splitlines())
+    commands = list(pairwise(lines))
+    commands_before = len(commands)
+    print(f"Processing {filename} with {commands_before} commands")
+    history: List[Tuple[str, str]] = [(command, date)
+                                      for date, command in commands
+                                      if len(command) >= minlen]
     if minlen:
-        shorts = 0
-        print(f"Removing commands shorter than {minlen} characters...")
-        for i in range(len(history) - 1, -1, -1):
-            if len(history[i][0]) < minlen:
-                del history[i]
-                shorts += 1
-        print(f"Removed {shorts} commands {minlen-1} chars or shorter.")
-    remove_dups(history)
-    print(f"De-duplicated history has {len(history)} commands.")
-    save_to_file(history, filename)
+        print(f"Removed {commands_before - len(history)} commands "
+              f"shorter than {minlen} characters")
+    dhistory = remove_dups(history)
+    print(f"De-duplicated history has {len(dhistory)} commands.")
+    save_to_file(dhistory, filename)
 
 
 if __name__ == '__main__':
